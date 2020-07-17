a9bf97374100b914f2604c743a95884f5d0dac82
KDev <kumaresh.devarajan@gmail.com>
2020-07-17 10:28:42 +0530

    functions..and other stuffs

---
+++ day03-s1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 08:13:38 2020
+
+@author: kudevara
+"""
+
+"""def rect_area(l=2, b=5):
+    return l * b
+
+print(rect_area(10,10))
+
+def average(*avrg):
+    return sum(avrg) / len(avrg)
+
+print(average(5, 10 ,15))
+
+print(average(5, 10 ,15,5, 10 ,15,5, 10 ,15))
+
+grades=[5, 10 ,15,5, 10 ,15,5, 10 ,15]
+
+print(average(*grades))
+
+print(average(*range(1,10,2)))
+
+# scope
+
+import statistics as stat
+
+
+
+e = 'hello'
+
+from math import sqrt
+
+from math import * # RISKY
+
+print(e) """
+
+# Data science
+
+lst = [1, 3,4,2,6,5,3,4,5,2]
+
+import statistics
+
+print(statistics.mean(lst))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
