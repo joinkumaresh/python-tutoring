9ae78eb91667eb55da14eb56ab23020b4410cc05
KDev <kumaresh.devarajan@gmail.com>
2020-07-29 11:42:03 +0530

    day06 more lists and tuples

---
+++ day06.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 08:32:06 2020
+
+@author: kudevara
+"""
+
+
+# letters = 'asdf'
+
+# asc_letters = sorted(letters)
+# print(asc_letters)
+
+# colors = ('r', 'o', 'y', 'g', 'b')
+# asc_colors = sorted(colors, reverse=True)
+# print(asc_colors)
+
+# search
+
+num = [3,7,1,4,2,8,5,6]
+# num *= 3
+# print(num)
+# print(num.index(5, 15))
+
+# print(num.index(4,4,12))
+
+# print(num.index(4, 0, len(num)))
+
+# key = 7
+
+# if key in num:
+#     print(f'found {key} at {num.index(key)}' )
+# else:
+#     print('not found')    
+
+# if 44 in num:
+#     print(f'found {key} at {num.index(key)}')
+# else:
+#     print('not found')    
+    
+    
+    # print(num[100]) #error
+
+# list methods:-
+
+# num.insert(0, 'red')
+# print(num)
+# num2= [1,2,3,4]
+# num.insert(0, 'orange')
+# print(num)
+
+# num.append('blue') 
+# print(num)
+
+# num.extend(num2)
+# print(num)
+
+
+cube = [ (item, item**3) for item in range(1,6) ]
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
