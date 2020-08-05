0c7f9d0206ba2f7e980a7edf24fbc65cd0a5a4a3
KDev <kumaresh.devarajan@gmail.com>
2020-08-05 21:40:44 +0530

    initial commit for Y2and3

---
+++ Year2-and-3/day01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 19:46:57 2020
+
+@author: kudevara
+"""
+
+
+# fundamental (if/else , looping, func, modules, libs)
+# list & tuple
+# strings
+# dictionatires
+# functions
+# oops
+# NLP
+# ML / DL
+# BigData intro
+# API
+
+
+print('Hello World !')
+print('Hello Andramoda !')
+
---
+++ Year2-and-3/test.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 20:23:30 2020
+
+@author: kudevara
+"""
+
+
+# find min of 3 nos
+
+# i/p and o/p
+# params to exec code
+
+# understand the req
+# 
+# don't write code
+# because !req => wrong sw
+
+# n1 = int(input('Enter n1'))
+# n2 = int(input('Enter n2'))
+# n3 = int(input('Enter n3'))
+
+# min = n1
+
+# if n2 < min:
+#     min = n2
+# if n3 < min:
+#     min = n3
+
+# print('the min value is : ', min)    
+
+# find total and avg for math in class
+
+# initialize
+
+# total = 0
+# grade_counter = 0
+# grades = [90, 91, 92 ,93, 94, 95, 96 , 97, 98]
+
+# # Processing
+
+# for grade in grades:
+#     total = total + grade
+#     grade_counter += 1
+
+# # terminate
+
+# avg = total / grade_counter
+# print('total is : ', total)
+# print('avg is : ', avg)
+
+    
+# # escape chars
+
+# print()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
